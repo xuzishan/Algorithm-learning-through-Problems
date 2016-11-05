5c5514a381eb08a2adc5abb156461ad7ba99bcae
XTao <vertextao@hotmail.com>
2016-11-05 10:16:15 +0800

    convert to utf8

--- cpp/utility/maxflow.cpp
+++ cpp/utility/maxflow.cpp
@@ -2,8 +2,8 @@
 //  maxflow.cpp
 //  laboratory
 //
-//  Created by Ğì×ÓÉº on 15/6/15.
-//  Copyright (c) 2015Äê xu_zishan. All rights reserved.
+//  Created by å¾å­çŠ on 15/6/15.
+//  Copyright (c) 2015å¹´ xu_zishan. All rights reserved.
 //
 #include "maxflow.h"
 Network::Network(int n): Graph<int>(n){
